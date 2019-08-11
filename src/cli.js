07a1d848e354ae4cc82dec6586a5664aa8eaf74b
Andy Chen <andycandomore@gmail.com>
2019-08-11 14:11:31 -0700

    tend to deploy

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 `use strict`
 import meow from 'meow'
-// import init from './init'
+import init from './init'
 import exportFnc from './export'
 import devFunc from './dev'
 /**
@@ -12,17 +12,33 @@ const cli = meow(`
   $ deck <action>
         export
   
+  Options
+  --src, -S  source directory
+  --out, -O  output directory
+  
   Examples
   $ deck
   $ deck export
-`, {})
+  $ export --src ./my_deck --output ./ready
+`, {
+  flags: {
+    src: {
+      type: 'string',
+      alias: 'S'
+    },
+    output: {
+      type: 'string',
+      alias: 'O'
+    }
+  }
+})
 
 switch (cli.input[0]) {
-  // case 'init':
-  //   init()
-  //   break
+  case 'init':
+    init();
+    break
   case 'export':
-    exportFnc({init: true, ...flags})
+    exportFnc({init: true, ...cli.flags})
     break
   default:
     devFunc()
--- src/init.js
+++ src/init.js
@@ -1,11 +1,13 @@
 import path from 'path'
 const exec = require('child_process').exec
+import {DEFAULT_SRC_FOLDER, DEFAULT_OUTPUT_FOLDER} from './default';
 
-const init = (flags) => {
-  const {src} = flags
-  const srcFolder = src?src:'decks'
-  const templateFoler = path.resolve(__dirname, 'templates')
-  exec(`cp -rf ${templateFoler}/ ./${srcFolder}`)
+const init = () => {
+  exec(`mkdir -p ${DEFAULT_SRC_FOLDER}`)
+  exec(`touch ${DEFAULT_SRC_FOLDER}/presentation.md`)
+  exec(`# You presentation starts here > ${DEFAULT_SRC_FOLDER}/presentation.md`)
+  console.log(`created ${DEFAULT_SRC_FOLDER}/presentation.md`)
+  console.log(`try \`deck\` to start`)
 }
 
 export default init
