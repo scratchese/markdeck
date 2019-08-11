484b57d386647f754fd35fcecb2fbb63eb34b57d
Andy Chen <andycandomore@gmail.com>
2019-08-11 14:27:13 -0700

    tend to deploy

--- src/init.js
+++ src/init.js
@@ -1,11 +1,11 @@
-import path from 'path'
-const exec = require('child_process').exec
+import path from 'path';
+import {exec} from 'child_process';
 import {DEFAULT_SRC_FOLDER, DEFAULT_OUTPUT_FOLDER} from './default';
 
 const init = () => {
   const DEFAULT_MD = `./${DEFAULT_SRC_FOLDER}/presentation.md`;
   exec(`mkdir -p ${DEFAULT_SRC_FOLDER}`)
-  exec(`cp -rf ${path.join(__dirname, 'markdeck', init.md)} ${DEFAULT_MD}`)
+  exec(`cp -rf ${path.join(__dirname, 'template', 'markdeck', 'init.md')} ${DEFAULT_MD}`)
   console.log(`created ${DEFAULT_MD}`)
   console.log(`try \`deck\` to start`)
 }
