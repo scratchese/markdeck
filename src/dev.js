bfafc81cb37b83bc9f4c1552cf241cd05410c111
Andy Chen <andycandomore@gmail.com>
2019-08-11 15:29:55 -0700

    tend to deploy

--- src/dev.js
+++ src/dev.js
@@ -1,6 +1,19 @@
 import fs from 'fs';
 import exportFnc from './export';
-import devServer from './dev-server';
+// import devServer from './dev-server';
+import staticServer from 'node-static';
+import http from 'http';
+
+const devServer = (srcFolder, port) => {
+  const server = new(staticServer.Server)(srcFolder);
+  const app = http.createServer((req,res) => {
+    req.addListener('end', function () {
+      server.serve(req, res);
+    }).resume()
+  }).listen(port, () => {
+    console.log(`[server] Serving ${srcFolder}/ on http://localhost:${port}`)
+  });
+}
 
 const callback = () => {
   console.log('[deck] files are up to date')
