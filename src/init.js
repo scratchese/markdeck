46746ca7db32fc22aef426606fef64d06a91ed35
Andy Chen <andycandomore@gmail.com>
2019-08-11 14:29:16 -0700

    tend to deploy

--- src/init.js
+++ src/init.js
@@ -5,7 +5,8 @@ import {DEFAULT_SRC_FOLDER, DEFAULT_OUTPUT_FOLDER} from './default';
 const init = () => {
   const DEFAULT_MD = `./${DEFAULT_SRC_FOLDER}/presentation.md`;
   exec(`mkdir -p ${DEFAULT_SRC_FOLDER}`)
-  exec(`cp -rf ${path.join(__dirname, 'markdeck', 'template', 'init.md')} ${DEFAULT_MD}`)
+  console.log(`cp -rf ${path.resolve(__dirname, 'markdeck', 'template', 'init.md')} ${DEFAULT_MD}`)
+  exec(`cp -rf ${path.resolve(__dirname, 'markdeck', 'template', 'init.md')} ${DEFAULT_MD}`)
   console.log(`created ${DEFAULT_MD}`)
   console.log(`try \`deck\` to start`)
 }
