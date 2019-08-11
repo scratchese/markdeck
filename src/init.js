e1c925fd69a0f3b3e05056c276a44a4d5ff16168
Andy Chen <andycandomore@gmail.com>
2019-08-11 14:19:04 -0700

    tend to deploy

--- src/init.js
+++ src/init.js
@@ -3,16 +3,17 @@ const exec = require('child_process').exec
 import {DEFAULT_SRC_FOLDER, DEFAULT_OUTPUT_FOLDER} from './default';
 
 const init = () => {
+  const DEFAULT_MD = `./${DEFAULT_SRC_FOLDER}/presentation.md`;
   exec(`mkdir -p ${DEFAULT_SRC_FOLDER}`)
-  exec(`touch ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  exec(`echo # You presentation > ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  exec(`echo # --- >> ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  exec(`echo # Second page >> ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  exec(`echo # --- >> ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  exec(`echo # check it out >> ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  exec(`echo https://github.com/amazingndyyy/markdeck >> ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  exec(`echo # --- >> ${DEFAULT_SRC_FOLDER}/presentation.md`)
-  console.log(`created ${DEFAULT_SRC_FOLDER}/presentation.md`)
+  exec(`touch ${DEFAULT_MD}`)
+  exec(`echo \# You presentation > ${DEFAULT_MD}`)
+  exec(`echo \\n --- \\n >> ${DEFAULT_MD}`)
+  exec(`echo \# Second page >> ${DEFAULT_MD}`)
+  exec(`echo \\n --- \\n >> ${DEFAULT_MD}`)
+  exec(`echo \# check it out >> ${DEFAULT_MD}`)
+  exec(`echo https://github.com/amazingndyyy/markdeck >> ${DEFAULT_MD}`)
+  exec(`echo \\n --- \\n >> ${DEFAULT_MD}`)
+  console.log(`created ${DEFAULT_MD}`)
   console.log(`try \`deck\` to start`)
 }
 
