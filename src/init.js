70dd20c1d588276a091c29e8675d6c7fd4dacd01
Andy Chen <andycandomore@gmail.com>
2019-08-11 14:26:01 -0700

    tend to deploy

--- src/init.js
+++ src/init.js
@@ -5,14 +5,7 @@ import {DEFAULT_SRC_FOLDER, DEFAULT_OUTPUT_FOLDER} from './default';
 const init = () => {
   const DEFAULT_MD = `./${DEFAULT_SRC_FOLDER}/presentation.md`;
   exec(`mkdir -p ${DEFAULT_SRC_FOLDER}`)
-  exec(`touch ${DEFAULT_MD}`)
-  exec(`echo \# You presentation > ${DEFAULT_MD}`)
-  exec(`echo \\n --- \\n >> ${DEFAULT_MD}`)
-  exec(`echo \# Second page >> ${DEFAULT_MD}`)
-  exec(`echo \\n --- \\n >> ${DEFAULT_MD}`)
-  exec(`echo \# check it out >> ${DEFAULT_MD}`)
-  exec(`echo https://github.com/amazingndyyy/markdeck >> ${DEFAULT_MD}`)
-  exec(`echo \\n --- \\n >> ${DEFAULT_MD}`)
+  exec(`cp -rf ${path.join(__dirname, 'markdeck', init.md)} ${DEFAULT_MD}`)
   console.log(`created ${DEFAULT_MD}`)
   console.log(`try \`deck\` to start`)
 }
---
+++ src/markdeck/init.md
@@ -0,0 +1,15 @@
+# Markdeck made simplism
+
+## Simple, fast, SEO ready
+
+---
+
+# Even better
+
+## Every slide is SEO ready
+
+---
+
+# Open source
+
+[Github](https://github.com/amazingndyyy/markdeck)
